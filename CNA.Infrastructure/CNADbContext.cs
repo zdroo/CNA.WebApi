15bd17303bd02c90aa0e7cacce763ec1a4918a33
Alex Zdru <59793697+zdroo@users.noreply.github.com>
2026-03-27 21:04:53 +0200

    adjust db context, add all configs

--- CNA.Infrastructure/CNADbContext.cs
+++ CNA.Infrastructure/CNADbContext.cs
@@ -10,39 +10,42 @@ namespace CNA.Infrastructure
         public CNADbContext(DbContextOptions<CNADbContext> options)
             : base(options) { }
 
-        public DbSet<Category> Categories => Set<Category>();
-        public DbSet<Product> Products => Set<Product>();
-        public DbSet<ProductVariant> ProductVariants => Set<ProductVariant>();
-        public DbSet<VariantAttribute> VariantAttributes => Set<VariantAttribute>();
-        public DbSet<Stock> Stocks => Set<Stock>();
-        public DbSet<User> Users => Set<User>();
-        public DbSet<Review> Reviews => Set<Review>();
-        public DbSet<Order> Orders => Set<Order>();
-        public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+        public DbSet<Address> Addresses => Set<Address>();
         public DbSet<Cart> Carts => Set<Cart>();
         public DbSet<CartItem> CartItems => Set<CartItem>();
-        public DbSet<ShippingContact> ShippingContacts => Set<ShippingContact>();
+        public DbSet<Category> Categories => Set<Category>();
         public DbSet<Country> Countries => Set<Country>();
-        public DbSet<Address> Addresses => Set<Address>();
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<OrderItem> OrderItems => Set<OrderItem>();
         public DbSet<Payment> Payments => Set<Payment>();
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<ProductVariant> ProductVariants => Set<ProductVariant>();
+        public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
+        public DbSet<Review> Reviews => Set<Review>();
+        public DbSet<ShippingContact> ShippingContacts => Set<ShippingContact>();
+        public DbSet<Stock> Stocks => Set<Stock>();
+        public DbSet<User> Users => Set<User>();
+        public DbSet<VariantAttribute> VariantAttributes => Set<VariantAttribute>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.ApplyConfiguration(new ProductVariantConfiguration());
-            modelBuilder.ApplyConfiguration(new ProductConfiguration());
-            modelBuilder.ApplyConfiguration(new ReviewConfiguration());
-            modelBuilder.ApplyConfiguration(new StockConfiguration());
-            modelBuilder.ApplyConfiguration(new VariantAttributeConfiguration());
-            modelBuilder.ApplyConfiguration(new OrderConfiguration());
-            modelBuilder.ApplyConfiguration(new OrderItemConfiguration());
+            modelBuilder.ApplyConfiguration(new AddressConfiguration());
             modelBuilder.ApplyConfiguration(new CartConfiguration());
             modelBuilder.ApplyConfiguration(new CartItemConfiguration());
-            modelBuilder.ApplyConfiguration(new UserConfiguration());
-            modelBuilder.ApplyConfiguration(new ShippingContactConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
             modelBuilder.ApplyConfiguration(new CountryConfiguration());
-            modelBuilder.ApplyConfiguration(new AddressConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderItemConfiguration());
             modelBuilder.ApplyConfiguration(new PaymentConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductVariantConfiguration());
+            modelBuilder.ApplyConfiguration(new RefreshTokenConfiguration());
+            modelBuilder.ApplyConfiguration(new ReviewConfiguration());
+            modelBuilder.ApplyConfiguration(new ShippingContactConfiguration());
+            modelBuilder.ApplyConfiguration(new StockConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new VariantAttributeConfiguration());
         }
     }
 }
